6af4ffdd71e4d98b526826259751412f1ca00ae2
eploteacher <59019569+Maxx009@users.noreply.github.com>
2020-12-05 13:39:04 +0530

    python codes uploaded

---
+++ python/Learn python/leap_year.py
@@ -0,0 +1,19 @@
+year = int(input("Enter the year:"))
+
+if(year % 4 == 0):				#condition to find leap year.
+
+	print "The given year is leap."
+
+else:
+
+	print "The given year is not leap."
+
+
+#OUTPUT
+
+#Enter the year:2020
+#The given year is leap.
+
+#Enter the year:2019
+#The given year is not leap.
+
---
+++ python/TURTLE/turt.py
@@ -0,0 +1,19 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.up()
+t.left(90)
+t.forward(250)
+t.left(90)
+t.forward(250)
+t.left(90)
+t.down(90)
+t.forward(400)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(400)
+
